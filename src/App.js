d9cdb6947d8b5b689ed7ed45b544fe785d27c783
Zeek <mzulkiful@yahoo.com>
2022-09-17 15:46:04 +0100

    Card component created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
 
 const cardImages = [
  {"src": "/img/helmet-1.png"},
@@ -30,16 +31,12 @@ console.log(cards, turns);
     <div className="App">
      <h1>Magic Match</h1>
      <button onClick={shuffleCards}>New Game</button>
+      
      <div className='card-grid'>
-        {cards.map(card => (
-          <div className='card' key={card.id}>
-            <div>
-              <img className='front' src= {card.src} alt = 'card front' />
-              <img className='back' src='/img/cover.png' alt='card back'/>
-            </div>
-          </div>
-        ))}
-     </div>
+            {cards.map(card => (
+            <SingleCard key={card.id} card = {card} />
+            ))}
+         </div> 
     </div>
   );
 }
---
+++ src/components/SingleCard.js
@@ -0,0 +1,17 @@
+import './SingleCard.css'
+
+
+ function SingleCard({card})  {
+
+        return (
+        <div className='card'>
+            <div>
+              <img className='front' src= {card.src} alt = 'card front' />
+              <img className='back' src='/img/cover.png' alt='card back'/>
+            </div>
+        </div>
+        )
+
+}
+
+export default SingleCard;
