6e3f4f937cb0ab8aafbde9bba8f3d5c5edc069a2
Zeek <mzulkiful@yahoo.com>
2022-09-17 17:13:02 +0100

    Comparing the selected cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import SingleCard from './components/SingleCard';
 
@@ -28,9 +28,35 @@ const shuffleCards = () => {
   setTurns(0)  
 }
 
+// handle choice 
 const handleChoice = (card)  => {
 choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
 
+}
+
+// conpare 2 selected card
+
+useEffect (() => {
+
+  if (choiceOne && choiceTwo) {
+
+    if (choiceOne.src === choiceTwo.src) {
+      console.log('those card match')
+      resetTurn()
+    } else {
+      console.log('those cards do not match')
+      resetTurn()
+    }
+  }
+
+}, [choiceOne, choiceTwo])
+// reset choices & increase turn
+
+const resetTurn = () => {
+
+  setChoiceOne(null)
+  setChoiceTwo(null)
+  setTurns(prevTurns => prevTurns + 1)
 }
 
   return (
